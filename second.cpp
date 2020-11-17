b0f124a5304470d3706809a5e6141801c8202566
Nikhil <54837275+Nikhil8847@users.noreply.github.com>
2020-11-17 20:39:29 +0530

    SecondSolution

---
+++ second.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+// Approach is to find anagrams by windows sliding method and checking if the two strings matches in their sorted form.
+void FindSubStrings(string bigger, string smaller){
+    string temp2 = smaller;
+    
+    sort(temp2.begin(), temp2.end());
+    
+    if(smaller.size() > bigger.size())
+    {
+        cout << "Inavlid input" << endl;
+        return;
+    }
+
+    for(int i = 0; i<= bigger.size()-smaller.size(); i++)
+    {
+        string temp = bigger.substr(i,smaller.size());
+        
+        sort(temp.begin(), temp.end());
+
+        if(temp == temp2){
+            cout << "String " << temp << " found at : " << i << " index" << endl;
+        }
+    }
+}
+
+int main(){
+    string bigger, smaller;
+    cout << "Enter the two strings (bigger than smaller respectively) : " << endl;
+    cin >> bigger >> smaller;
+    FindSubStrings(bigger, smaller);
+    return 0;
+}
